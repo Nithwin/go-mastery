017c7f27260761a67e82cbf400e2e6ef22409621
Nithwin <vmnithwin@gmail.com>
2026-07-08 23:02:21 +0530

    feat: add variadic sum function and update main to demonstrate usage

--- 01-basics/calculator/main.go
+++ 01-basics/calculator/main.go
@@ -19,10 +19,21 @@ func divide(x, y int) int {
 	return x + y;
 }
 
+func sum(numbers ...int) int {
+	res := 0;
+	for _, n := range numbers {
+		res += n;
+	}
+
+	return res;
+}
+
 func main(){
 
 	fmt.Println(add(4,5));
 	fmt.Println(subtract(7,5));
 	fmt.Println(multiply(5,5));
 	fmt.Println(divide(20,4));
+	fmt.Println(sum(5,6,7,8));
+	fmt.Println(sum(1,2,3,4,5,6,7,8));
 }

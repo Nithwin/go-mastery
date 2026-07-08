641f395ec61a2ed44565493c562c1d39e2ec5b97
Nithwin <vmnithwin@gmail.com>
2026-07-08 22:30:54 +0530

    feat: add user input demonstration program to capture and print name and age

---
+++ 01-basics/user-input/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string;
+	var age int;
+	fmt.Print("Enter your name and age: ");
+	fmt.Scan(&name, &age);
+
+	fmt.Println("Hello ", name, " your age is ", age);
+}
+

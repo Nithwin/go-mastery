a9086fb6e44b241927e325866a4c12d23b00f4dc
Nithwin <vmnithwin@gmail.com>
2026-07-08 21:15:09 +0530

    feat: initialize hello-go project with main module and global gitignore configuration

---
+++ 01-basics/hello-go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World!");
+	fmt.Println("Hello, Nithwin!");
+	fmt.Println("Welcome to Go.");
+	fmt.Println("I will build Stormbreaker.")
+}

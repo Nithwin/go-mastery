9f3f792ad5781f04797817f3438f9cc7b8e640e3
Nithwin <vmnithwin@gmail.com>
2026-07-13 21:56:48 +0530

    feat: add map demonstration module with initial main implementation

---
+++ 01-basics/mapp/go.mod
@@ -0,0 +1,3 @@
+module mapp
+
+go 1.26.3
---
+++ 01-basics/mapp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	ages := map[string]int {
+		"Nithwin": 21,
+		"Monkey D Luffy": 19,
+	}
+
+	fmt.Println(ages);
+}
